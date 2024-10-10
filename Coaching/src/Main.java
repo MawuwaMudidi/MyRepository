e8cff26d41197eafde1f1850cf0b4fee840e59ab
Maua <mawuwamudidi@gmail.com>
2024-10-10 16:09:49 +0300

    task find min operation when dividing by 3

--- Coaching/src/Main.java
+++ Coaching/src/Main.java
@@ -15,8 +15,10 @@ public class Main {
 
         FourDoorToyota toyota = new FourDoorToyota("T1-001", "Camry", "Red", 30000, 200, 50, 2023, 0,40);
 
-        Dealer dealer = new Dealer("Joe", 100000);
+        Dealer dealer = new Dealer("Joe", "Kampala", "0778899221", 10000);
         dealer.dealerMenu();
+
+        Dealers newDealer = new Dealers(dealer.name, dealer.location, dealer.contact, dealer.carPrice);
 
     }
 }
